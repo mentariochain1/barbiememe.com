66363b294a804f3b14d9bd6f555ffe521148f970
tarrant <tarrant.harris@icloud.com>
2024-09-15 05:06:34 +0300

    Исправлена загрузка шрифтов и обновлены стили

--- .history/src/components/BackgroundSwitcher_20240915050140.tsx
+++ .history/src/components/BackgroundSwitcher_20240915050140.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 interface BackgroundSwitcherProps {
   children: React.ReactNode;
-  isCalmBackground?: boolean;
+  isCalmBackground?: boolean; // Добавьте это свойство в интерфейс
 }
 
 const BackgroundSwitcher: React.FC<BackgroundSwitcherProps> = ({ children, isCalmBackground }) => {
+  // Используйте isCalmBackground в логике компонента, а не передавайте его напрямую в div
   return (
     <div className={isCalmBackground ? 'calm-background' : 'dynamic-background'}>
       {children}
